67ada844452294d9d0d04702d07ada9915b86a7b
Marcus Valério <65788029+marcusvalerio@users.noreply.github.com>
2026-09-02 07:21:15 -0300

    fix: tornar exportacao PNG compativel com mobile

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './app/App'
 import './app/index.css'
+import { installPngDownloadFallback } from './shared/lib/mobilePngDownloadFallback'
+
+installPngDownloadFallback()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
